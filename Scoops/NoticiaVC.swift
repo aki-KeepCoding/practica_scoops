f17f28cbd2e91b7b5e571f09d2cee1decf2789cb
AOA <akixe.otegi@sgsmap.com>
2016-10-26 18:52:16 +0200

    Añade Vista de Noticia de Editor y enlaza con Tabla de Noticias de editor

---
+++ Scoops/NoticiaVC.swift
@@ -0,0 +1,46 @@
+//
+//  NoticiaVC.swift
+//  Scoops
+//
+//  Created by Akixe on 26/10/16.
+//  Copyright © 2016 AOA. All rights reserved.
+//
+
+import UIKit
+
+class NoticiaVC: UIViewController {
+
+    let model : Noticia
+    
+    init(model:Noticia) {
+        self.model = model
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
